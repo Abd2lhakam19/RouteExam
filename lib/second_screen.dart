75827a7c0d3de1877f47e7b3a21ef843a5611dfe
Mahmo <midoahme93@gmail.com>
2023-09-27 12:03:47 +0300

    Initial Commit

---
+++ lib/second_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+ static const String routeName = "route";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
